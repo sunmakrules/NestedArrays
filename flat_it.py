4dea82a6bb20e4636e572e761f251c16c471ea89
Sandeep Makhija <sandeep.makhija@nectechnologies.in>
2016-05-04 19:08:27 +0530

    Update flat_it.py

--- flat_it.py
+++ flat_it.py
@@ -1,6 +1,7 @@
+#function to flatten a nested array
 flatten = lambda x: [y for l in x for y in flatten(l)] if type(x) is list else [x]
 
 a = [1, 2, [3, 4], [[5, 6], [7, 8]]]
-print(flatten(a))
+print(flatten(a)) #Calls flatten function
 
 
